c0771bc8255488d531427d5de2e5b1c640b88abd
Jose Francisco <junior.poster@gmail.com>
2020-07-15 23:31:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# <PROJECT NAME>
+This project was entirely developed using the framework Angela.js. 
+For running this project you'll only need Node/npm installed
+However, for your convinience, it is advised that you install Angela.js at ....................
+
+## Installation Steps
+
+## Running the app
+
+## Running tests
+
+# REST API
+The REST API to the <PROJECT NAME> app is described below
+
+## Request 
---
+++ routes/home.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+
+const puppeteer = require("puppeteer");
+log = console.log;
+
+getExchangeRate = (cb) => {
+  (async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto("http://www.kinguilahoje.com/");
+
+    const dollar = await page.$eval(
+      "#page-top > header > div > div > div.row.margin-top-0px > div > div > div:nth-child(1) > span.col-xs-12.quotation",
+      (element) => element.textContent
+    );
+
+    const euro = await page.$eval(
+      "#page-top > header > div > div > div.row.margin-top-0px > div > div > div:nth-child(2) > span.col-xs-12.quotation",
+      (element) => element.textContent
+    );
+
+    await page.screenshot({ path: "example.png" });
+    await browser.close();
+
+    cb({ EUR: euro, USD: dollar });
+  })();
+};
+
+router.get("/", (req, res) => {
+  getExchangeRate((rates) => {
+    res.json.send(rates);
+  });
+});
+
+module.exports = router;
